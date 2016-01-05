50246ce302fd408924fbe8f134aaea7e06860ea2
David Sancho <dsnxmoreno@gmail.com>
2016-01-05 16:16:13 +0100

    Add o-prefix for FequestAnim

--- scrollto-with-animation.js
+++ scrollto-with-animation.js
@@ -1,11 +1,13 @@
 ;(function (window) {
   var requestAnimFrame = (function () {
-    return window.requestAnimationFrame ||
-    window.webkitRequestAnimationFrame ||
-    window.mozRequestAnimationFrame ||
-    function (callback) {
-      window.setTimeout(callback, 1000 / 60)
-    }
+    return
+      window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.oRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      function (callback) {
+        window.setTimeout(callback, 1000 / 60)
+      }
   })()
 
   var easings = {
