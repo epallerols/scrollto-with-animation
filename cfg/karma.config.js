9684f9857e4a3f90ee3cb66870e47b9307c5cf34
David Sancho <dsnxmoreno@gmail.com>
2016-02-08 10:23:59 +0100

    Implement first test of findAnimation

--- cfg/karma.config.js
+++ cfg/karma.config.js
@@ -1,28 +1,23 @@
-// const webpackcfg = require('./../cfg/webpack.config')
+const webpackcfg = require('./../cfg/webpack.config')
 const path = require('path')
 const ROOT_PATH = path.join(__dirname, '..')
 
 module.exports = function (config) {
   config.set({
-    // list of files to exclude
     exclude: [],
-    // list of files / patterns to load in the browser
+    basePath: ROOT_PATH,
     files: [
-      ROOT_PATH + '/node_modules/phantomjs-polyfill/bind-polyfill.js',
-      ROOT_PATH + '/test/*'
+      '/node_modules/phantomjs-polyfill/bind-polyfill.js',
+      'test/**/*.js'
     ],
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'test/*': ['webpack', 'sourcemap']
+      'test/**/*.js': ['webpack', 'sourcemap']
     },
-    // webpack: {
-    //   module: webpackcfg.module
-    // },
+    webpack: {
+      module: webpackcfg.module,
+      process: false
+    },
     plugins: [
       'karma-jasmine',
       'karma-webpack',
@@ -32,30 +27,14 @@ module.exports = function (config) {
       'karma-spec-reporter',
       'karma-coverage'
     ],
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera (has to be installed with `npm install karma-opera-launcher`)
-    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
-    // - PhantomJS
-    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
     browsers: ['PhantomJS'],
     browserDisconnectTimeout: 10000,
     browserDisconnectTolerance: 2,
-    // If browser does not capture in given timeout [ms], kill it
     captureTimeout: 100000,
     browserNoActivityTimeout: 30000,
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
     logColors: true,
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false
-  // Continuous Integration mode
-  // if true, Karma captures browsers, runs the tests and exits
-  // singleRun: !process.env.CONTINUOUS_INTEGRATION
   })
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,19 @@
-// const TO = 0
-// const DURATION = 100
-// const TRANSITION_OK = 'easeInQuad'
-// const TRANSITION_KO = 'flowInOutFly'
-// const CB = function () {}
+import scrollto from './../src/index'
 
-describe('', function () {
-  it('', function () {
-    expect(true).toEqual(true)
+const TO = 0
+const DURATION = 100
+const TRANSITION_OK = 'easeInOutQuad'
+const TRANSITION_KO = 'flowInOutFly'
+const CB = function () {}
+
+describe('#findAnimation', function () {
+  it(`to throw an exception when transition isn't correct`, function () {
+    const func = () => scrollto(document, TO, DURATION, TRANSITION_KO)
+    expect(func).toThrow()
+  })
+
+  it(`to not throw an exception when transition is correct`, function () {
+    const func = () => scrollto(document, TO, DURATION, TRANSITION_OK)
+    expect(func).not.toThrow()
   })
 })
