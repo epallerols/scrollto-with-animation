1516b64dbd9087cb19bafafdb0164db2b6887fca
David Sancho <dsnxmoreno@gmail.com>
2016-02-08 10:24:14 +0100

    Clean test stuff

--- test/index.test.js
+++ test/index.test.js
@@ -1,19 +1,20 @@
 import scrollto from './../src/index'
 
-const TO = 0
-const DURATION = 100
-const TRANSITION_OK = 'easeInOutQuad'
-const TRANSITION_KO = 'flowInOutFly'
-const CB = function () {}
-
 describe('#findAnimation', function () {
+  beforeEach(function () {
+    this.TO = 0
+    this.DURATION = 100
+    this.TRANSITION_OK = 'easeInOutQuad'
+    this.TRANSITION_KO = 'flowInOutFly'
+  })
+
   it(`to throw an exception when transition isn't correct`, function () {
-    const func = () => scrollto(document, TO, DURATION, TRANSITION_KO)
+    const func = () => scrollto(document, this.TO, this.DURATION, this.TRANSITION_KO)
     expect(func).toThrow()
   })
 
   it(`to not throw an exception when transition is correct`, function () {
-    const func = () => scrollto(document, TO, DURATION, TRANSITION_OK)
+    const func = () => scrollto(document, this.TO, this.DURATION, this.TRANSITION_OK)
     expect(func).not.toThrow()
   })
 })
