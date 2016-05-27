a2a0fabddd8766b038a9862f95ba33463e64da20
David Sancho <dsnxmoreno@gmail.com>
2016-05-27 13:50:49 +0200

    Move into the class

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import easings from './easings'
 import AnimationFrame from 'animation-frame'
-
 const animationFrame = new AnimationFrame()
+
 const DEFAULT_ANIMATION = 'easeInQuad'
 const LIB_NAME = 'scrollto-with-animation'
 const URL_GITHUB = 'https://github.com/davesnx/scrollToWithAnimation'
@@ -12,75 +12,75 @@ const TRANSITION_NOT_VALID = `${LIB_NAME}: Transition isn't string or Function -
 const _document = typeof document !== 'undefined' ? document : {}
 const _window = typeof window !== 'undefined' ? window : {}
 
-class scrollToWithAnimation {
+const findAnimation = (transition = DEFAULT_ANIMATION) => {
+  var animation = easings[transition]
+  if (animation === undefined) {
+    throw new Error(TRANSITION_NOT_FOUND)
+  }
+  return animation
+}
 
-  static findAnimation (transition = DEFAULT_ANIMATION) {
-    var animation = easings[transition]
-    if (animation === undefined) {
-      throw new Error(TRANSITION_NOT_FOUND)
-    }
-    return animation
+const defineAnimation = (transition) => {
+  if (transition.length !== 4) {
+    throw new TypeError(ANIMATION_NOT_VALID)
+  }
+  return transition
+}
+
+const scrollToWithAnimation = (
+  element = _document,
+  direction = 'scrollTop',
+  to = 0,
+  duration = 100,
+  transition = DEFAULT_ANIMATION,
+  callback
+) => {
+  let start = direction === 'scrollTop' ? element.scrollTop : element.scrollLeft
+  let change = to - start
+  let animationStart = +new Date()
+  let animating = true
+  let lastpos = null
+  let eq = null
+
+  if (typeof transition === 'string' || transition === null) {
+    eq = findAnimation(transition)
+  } else if (typeof transition === 'function') {
+    eq = defineAnimation(transition)
+  } else {
+    throw new TypeError(TRANSITION_NOT_VALID)
   }
 
-  static defineAnimation (transition) {
-    if (transition.length !== 4) {
-      throw new TypeError(ANIMATION_NOT_VALID)
-    }
-    return transition
-  }
-
-  static do (element = _document, direction = 'scrollTop', to = 0, duration = 100, transition = DEFAULT_ANIMATION, callback) {
-    let start = direction === 'scrollTop' ? element.scrollTop : element.scrollLeft
-    let change = to - start
-    let animationStart = +new Date()
-    let animating = true
-    let lastpos = null
-    let eq = null
-
-    if (typeof transition === 'string' || transition === null) {
-      eq = scrollToWithAnimation.findAnimation(transition)
-    } else if (typeof transition === 'function') {
-      eq = scrollToWithAnimation.defineAnimation(transition)
-    } else {
-      throw new TypeError(TRANSITION_NOT_VALID)
-    }
-
-    const animateScroll = function () {
-      if (!animating) {
-        return
-      }
-      animationFrame.request(animateScroll)
-      const now = +new Date()
-      const val = Math.floor(eq(now - animationStart, start, change, duration))
-      if (lastpos) {
-        if (lastpos === element[direction]) {
-          lastpos = val
-          element[direction] = val
-        } else {
-          animating = false
-        }
-      } else {
-        lastpos = val
-        element[direction] = val
-      }
-      if (now > animationStart + duration) {
-        element[direction] = to
-        animating = false
-        if (callback) {
-          callback()
-        }
-      }
+  const animateScroll = () => {
+    if (!animating) {
+      return
     }
     animationFrame.request(animateScroll)
+    const now = +new Date()
+    const val = Math.floor(eq(now - animationStart, start, change, duration))
+    if (lastpos) {
+      if (lastpos === element[direction]) {
+        lastpos = val
+        element[direction] = val
+      } else {
+        animating = false
+      }
+    } else {
+      lastpos = val
+      element[direction] = val
+    }
+    if (now > animationStart + duration) {
+      element[direction] = to
+      animating = false
+      if (callback) {
+        callback()
+      }
+    }
   }
-
+  animationFrame.request(animateScroll)
 }
 
-_window.scrollToWithAnimation = scrollToWithAnimation.do
+// Publish public method in window
+_window.scrollToWithAnimation = scrollToWithAnimation
 
-export default scrollToWithAnimation.do
-
-// Export this for unit testing purposes
-export {
-  animationFrame
-}
+export default scrollToWithAnimation
+export {animationFrame}
